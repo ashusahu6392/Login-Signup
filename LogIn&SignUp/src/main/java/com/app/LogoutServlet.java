257b6eb2a3f3cd76b674542f4040e61d91c865a7
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-01-12 18:12:54 +0530

    Add LogoutServlet for user session management
    Implement LogoutServlet to handle user logout and session invalidation.

--- LogIn&SignUp/src/main/java/com/app/LogoutServlet.java
+++ LogIn&SignUp/src/main/java/com/app/LogoutServlet.java
@@ -1,1 +1,35 @@
 
+package com.app;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+@WebServlet("/logoutServlet")
+public class LogoutServlet extends HttpServlet {
+	
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		  HttpSession session = req.getSession(false);
+	        if (session != null) {
+	            session.invalidate(); // 🔥 DESTROYS SESSION
+	        }
+		resp.sendRedirect("index.jsp");
+	}
+
+}
